fca2e007340c39e709cce2805a4f438b84c2d0e5
Sergey Matyukevich <geomatsi@gmail.com>
2015-05-29 00:14:08 +0300

    Add new demo: FreeRTOS + libopencm3
    Simple blink demo using FreeRTOS and libopencm3
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ boards/stm32f4-discovery/apps/freertos/build.mk
@@ -0,0 +1,69 @@
+#
+# example: FreeRTOS + libopencm3
+#
+
+FREERTOS_DIR = $(PRJ_DIR)/../FreeRTOSV7.3.0/FreeRTOS
+
+## paths
+
+VPATH += $(PRJ_DIR)/common
+VPATH += $(PRJ_DIR)/boards/stm32f4-discovery/bsp
+VPATH += $(PRJ_DIR)/boards/stm32f4-discovery/apps/freertos
+
+VPATH += $(FREERTOS_DIR)/Source
+VPATH += $(FREERTOS_DIR)/Source/portable/MemMang
+VPATH += $(FREERTOS_DIR)/Source/portable/GCC/ARM_CM4F
+
+## sources
+
+RTOS_SRCS := \
+	main.c \
+	stdlib.c \
+
+RTOS_SRCS += \
+	list.c \
+	queue.c \
+	tasks.c \
+	timers.c \
+	croutine.c \
+	heap_1.c \
+	port.c
+
+RTOS_OBJS := $(RTOS_SRCS:.c=.o)
+RTOS_OBJS := $(addprefix $(OBJ_DIR)/,$(RTOS_OBJS))
+
+## deps
+
+LIBS = $(LIBCM3)
+
+## flags
+
+CFLAGS  = $(PFLAGS) -Wall -O2 -DSTM32F4
+
+CFLAGS += -I$(PRJ_DIR)/include $(LIBCM3_INC)
+CFLAGS	+= -I$(PRJ_DIR)/boards/$(PLAT)/apps/freertos
+CFLAGS += -I$(FREERTOS_DIR)/Source/include -I$(FREERTOS_DIR)/Source/portable/GCC/ARM_CM4F
+
+LDFLAGS = -T$(PRJ_DIR)/ld/stm32f4-discovery.ld
+
+## rules
+
+freertos: $(OBJ_DIR)/freertos.bin
+	cp $(OBJ_DIR)/freertos.bin $(OBJ_DIR)/test.bin
+
+%.hex: %.elf
+	$(OBJCOPY) -O ihex $^ $@
+
+%.bin: %.elf
+	$(OBJCOPY) -O binary $^ $@
+
+$(OBJ_DIR)/freertos.elf: $(RTOS_OBJS) $(LIBS) $(PRJ_DIR)/ld/stm32f4-discovery.ld
+	$(LD) $(LDFLAGS) $(RTOS_OBJS) $(LIBS) -o $@
+
+$(OBJ_DIR)/%.o: %.c
+	mkdir -p $(dir $@)
+	$(CC) -c $(CFLAGS) -o $@ $^
+
+$(OBJ_DIR)/%.o: %.s
+	mkdir -p $(dir $@)
+	$(CC) -c $(CFLAGS) -o $@ $^
---
+++ boards/stm32f4-discovery/apps/freertos/main.c
@@ -0,0 +1,129 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
+
+#include "FreeRTOSConfig.h"
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+
+#include "clock.h"
+
+/* */
+
+uint32_t SystemCoreClock;
+
+/* */
+
+void xdelay(volatile uint32_t count)
+{
+	while(count) {
+		count--;
+	}
+}
+
+static void critical_error_blink(void)
+{
+	gpio_clear(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+
+	while (1) {
+		gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+		xdelay(0x3fffff);
+	}
+}
+
+static void test_blink(uint32_t pin, uint32_t bank, uint32_t num)
+{
+	gpio_clear(bank, pin);
+
+	while (num--) {
+		gpio_toggle(bank, pin);
+		xdelay(0x3fffff);
+	}
+
+	gpio_clear(bank, pin);
+}
+
+/* */
+
+static void hw_init(void)
+{
+	/* TODO: sync SystemCoreClock with rcc_clock_setup_xxx settings */
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_120MHZ]);
+	SystemCoreClock = 120000000;
+
+	rcc_periph_clock_enable(RCC_GPIOD);
+
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO14);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
+
+	gpio_clear(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+
+	/*
+		http://www.freertos.org/RTOS-Cortex-M3-M4.html
+		Preempt priority and subpriority:
+
+			If you are using an STM32 with the STM32 driver library then ensure all the
+			priority bits are assigned to be preempt priority bits by calling
+			NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); before the RTOS is started.
+	*/
+
+	scb_set_priority_grouping(SCB_AIRCR_PRIGROUP_GROUP16_NOSUB);
+
+	/* DON'T PUT ANYTHING HERE AFTER NVIC PRIO GROUP ARE PROPERLY CONFIGURED FOR FREERTOS */
+}
+
+static void LedFlash1(void *Parameters)
+{
+	uint32_t delay = (uint32_t) Parameters;
+
+	portTickType LastWake;
+
+	gpio_set(GPIOD, GPIO12);
+
+	LastWake = xTaskGetTickCount();
+
+	while(1) {
+		gpio_toggle(GPIOD, GPIO12);
+		vTaskDelayUntil(&LastWake, delay);
+	}
+}
+
+static void LedFlash2(void *Parameters)
+{
+	uint32_t delay = (uint32_t) Parameters;
+
+	portTickType LastWake;
+
+	gpio_set(GPIOD, GPIO13 | GPIO15);
+
+	LastWake = xTaskGetTickCount();
+
+	while(1) {
+		gpio_toggle(GPIOD, GPIO13 | GPIO15);
+		vTaskDelayUntil(&LastWake, delay);
+	}
+}
+
+int main(void)
+{
+	hw_init();
+
+	xTaskCreate(LedFlash1, (signed char *) "led1", configMINIMAL_STACK_SIZE, (void *) 1000, tskIDLE_PRIORITY + 2, NULL);
+	xTaskCreate(LedFlash2, (signed char *) "led2", configMINIMAL_STACK_SIZE, (void *) 1500, tskIDLE_PRIORITY + 3, NULL);
+
+	vTaskStartScheduler();
+
+	/* just sit and flash all the LEDs quickly if we fail */
+	critical_error_blink();
+	return 0;
+}
