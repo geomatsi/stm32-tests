848e8739372da405ab4800b63b1f8d8001e4c37c
Sergey Matyukevich <geomatsi@gmail.com>
2015-05-29 23:08:27 +0300

    Add user button handling into freertos demo
    Handle user button on stm32f4-discovery using both freertos and libopencm3 capabilities:
      - libopencm3: use EXTI subsystem to handle interrupt from user button
      - freertos: pass message via queue from exti0_isr to dedicated ButtonControl task
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- boards/stm32f4-discovery/apps/freertos/main.c
+++ boards/stm32f4-discovery/apps/freertos/main.c
@@ -6,6 +6,9 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/exti.h>
+
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/scb.h>
 
 #include "FreeRTOSConfig.h"
@@ -19,6 +22,8 @@
 /* */
 
 uint32_t SystemCoreClock;
+bool exti0_falling;
+xQueueHandle xQueue;
 
 /* */
 
@@ -53,6 +58,27 @@ static void test_blink(uint32_t pin, uint32_t bank, uint32_t num)
 
 /* */
 
+void exti0_isr(void)
+{
+	unsigned char msg;
+
+	exti_reset_request(EXTI0);
+
+	if (exti0_falling) {
+		msg = 'D';
+		xQueueSendToFrontFromISR(xQueue, &msg, NULL);
+		exti0_falling = false;
+		exti_set_trigger(EXTI0, EXTI_TRIGGER_RISING);
+	} else {
+		msg = 'E';
+		xQueueSendToFrontFromISR(xQueue, &msg, NULL);
+		exti0_falling = true;
+		exti_set_trigger(EXTI0, EXTI_TRIGGER_FALLING);
+	}
+}
+
+/* */
+
 static void hw_init(void)
 {
 	/* TODO: sync SystemCoreClock with rcc_clock_setup_xxx settings */
@@ -60,6 +86,9 @@ static void hw_init(void)
 	SystemCoreClock = 120000000;
 
 	rcc_periph_clock_enable(RCC_GPIOD);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
 
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
@@ -67,6 +96,17 @@ static void hw_init(void)
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
 
 	gpio_clear(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+
+	/* enable EXTI0 interrupt */
+
+	nvic_enable_irq(NVIC_EXTI0_IRQ);
+
+	/* configure the EXTI subsystem */
+
+	exti_select_source(EXTI0, GPIOA);
+	exti0_falling = false;
+	exti_set_trigger(EXTI0, EXTI_TRIGGER_RISING);
+	exti_enable_request(EXTI0);
 
 	/*
 		http://www.freertos.org/RTOS-Cortex-M3-M4.html
@@ -80,6 +120,30 @@ static void hw_init(void)
 	scb_set_priority_grouping(SCB_AIRCR_PRIGROUP_GROUP16_NOSUB);
 
 	/* DON'T PUT ANYTHING HERE AFTER NVIC PRIO GROUP ARE PROPERLY CONFIGURED FOR FREERTOS */
+}
+
+/* */
+
+static void ButtonControl(void *Parameters)
+{
+	unsigned char c;
+
+	gpio_clear(GPIOD, GPIO14);
+
+	while(1) {
+		xQueueReceive(xQueue, &c, 10000);
+
+		switch (c) {
+			case 'E':
+				gpio_set(GPIOD, GPIO14);
+				break;
+			case 'D':
+				gpio_clear(GPIOD, GPIO14);
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 static void LedFlash1(void *Parameters)
@@ -120,6 +184,9 @@ int main(void)
 
 	xTaskCreate(LedFlash1, (signed char *) "led1", configMINIMAL_STACK_SIZE, (void *) 1000, tskIDLE_PRIORITY + 2, NULL);
 	xTaskCreate(LedFlash2, (signed char *) "led2", configMINIMAL_STACK_SIZE, (void *) 1500, tskIDLE_PRIORITY + 3, NULL);
+
+	xQueue = xQueueCreate( 10, sizeof( unsigned char ) );
+	xTaskCreate(ButtonControl, (signed char *) "button", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
 
 	vTaskStartScheduler();
 
